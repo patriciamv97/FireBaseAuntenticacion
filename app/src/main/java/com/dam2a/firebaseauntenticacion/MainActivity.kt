e75fcbb94fabb74ea6daf2f56aab19dfa24f9a53
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-12-05 17:13:45 +0100

    Comentarios del código añadidos

--- app/src/main/java/com/dam2a/firebaseauntenticacion/MainActivity.kt
+++ app/src/main/java/com/dam2a/firebaseauntenticacion/MainActivity.kt
@@ -39,10 +39,12 @@ class MainActivity : AppCompatActivity() {
         //Instanciamos en objeto auth que nos permitira crear un usuario e iniciar sesion
         auth = Firebase.auth
 
-        registro.setOnClickListener{
+        /*Hacemos un listener de los botones para que cuando se pulsen llamen a la función createAccount e sgnIn que
+        Permitirá  crear una cuenta o inciar sesión cuando cuando se pulsen los botones*/
+        registro.setOnClickListener {
             createAccount(email.text.toString(), contraseña.text.toString())
         }
-        iniciarSesion.setOnClickListener{
+        iniciarSesion.setOnClickListener {
             signIn(email.text.toString(), contraseña.text.toString())
         }
 
@@ -52,22 +54,28 @@ class MainActivity : AppCompatActivity() {
      * Método que recibe un email y una contraseña las valida y crea un usuario con ellas.
      */
     private fun createAccount(email: String, password: String) {
-
+        /*
+            Si el registro del usuario  se realiza correctamente
+            el listener del estado de autenticación será notificado y la lógica para manejar el
+            el usuario que se ha registrado se puede resuelve en el listener.
+        */
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
+                    //Logs de control
                     Log.d(TAG, "createUserWithEmail:success")
-                    Log.d("estado","usuario registrado")
+                    Log.d("estado", "usuario registrado")
+                    //Asignas a la variable user el usuario actual  para después pasarselo a método updateUI
                     val user = auth.currentUser
+                    //Se llama al método que actualizará el layout cuando se haya registrado y se le pasa el usuario
                     updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Log.d("estado","usuario NO registrado")
-
+                    //  Si el registro falla, muestra un mensaje al usuario
                     Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    updateUI(null)
+                    //Logs de control
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Log.d("estado", "usuario NO registrado")
+
                 }
             }
 
@@ -76,55 +84,45 @@ class MainActivity : AppCompatActivity() {
     /**
      * Método que sirve para actualizar el layout cuando inicias sesión o te registras
      * Para que funcione deberias crear otras activities y que se cargasen cuando el registro
-     * o el inicio fuesen correcto y te permitiese acceder a las funcionalidades de la app
+     * o el inicio fuesen correctos y te permitiesen acceder a las funcionalidades de la app
      */
 
     private fun updateUI(user: FirebaseUser?) {
-        Log.d("estado",""+auth.currentUser?.uid)
+        Log.d("estado", "" + auth.currentUser?.uid)
     }
 
     /**
      * Método que sirve para iniciar sesión a través de un email y contraseña
      */
     private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
+        /*
+          Si el inicio de sesión del usuario  se realiza correctamente
+          el listener del estado de autenticación será notificado y la lógica para manejar el
+          el usuario que ha iniciado sesión se puede resuelve en el listener.
+      */
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    //Inicio de sesion correcto el metodo update
+                    //Logs de control
                     Log.d(TAG, "signInWithEmail:success")
-                    Log.d("estado","inicio de sesión correcto")
-
+                    Log.d("estado", "inicio de sesión correcto")
+                    //Asignas a la variable user el usuario actual para después pasarselo a método updateUI
                     val user = auth.currentUser
+                    //Se llama al método que actualizará el layout cuando se haya iniciado sesión y se le pasa el usuario
                     updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
+                    //  Si el inicio de sesión falla, muestra un mensaje al usuario
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Log.d("estado","No se puedo iniciar sesion")
-
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    updateUI(null)
+                    Log.d("estado", "No se puedo iniciar sesion")
                 }
             }
 
     }
-
-    /**
-     * Método que envia una vereficación a el correo del usuario
-     */
-    private fun sendEmailVerification() {
-
-        val user = auth.currentUser!!
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                // Email Verification sent
-            }
-
-    }
-
+   /*
+    Un companion object es un objeto que es común a todas las instancias de esa clase. Vendría a ser similar a los campos estáticos en Java.
+    En este caso creamos el objeto TAG al que llamaremos en los Log de control para saber si se ha iniciado sesión correctamente
+    */
     companion object {
         private const val TAG = "EmailPassword"
     }
